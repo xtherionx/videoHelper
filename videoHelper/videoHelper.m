b4b26b8c166c9d0570920d1fd833a0d1f2f7b20e
xtherionx <x_therion_x1@yahoo.com.mx>
2013-08-14 11:05:08 -0500

    Initial Commit

---
+++ videoHelper/videoHelper.m
@@ -0,0 +1,13 @@
+//
+//  videoHelper.m
+//  videoHelper
+//
+//  Created by Sergio Quintero Gómez on 8/14/13.
+//  Copyright (c) 2013 Sergio Quintero Gómez. All rights reserved.
+//
+
+#import "videoHelper.h"
+
+@implementation videoHelper
+
+@end
